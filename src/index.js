0fbfed6c3c949af495abb2ba6628196b7051038a
andoski99 <goodsportdevelopment@icloud.com>
2023-01-27 17:51:15 +1100

    Showing Manipulation of DOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { companiesData } from "./companies-data";
 
 const before = document.getElementById("before");
-before.innerHTML = JSON.stringify(companiesData, null, 2);
+const after = document.getElementById("after");
 
-const manipulatedData = [];
+const manipulatedData = [1,2,3,4,5,6,7,8,9,0];
+
+before.innerHTML = JSON.stringify(companiesData, null, 2);
+after.innerHTML = JSON.stringify(manipulatedData, null, 2);
+
 //TODO: add the manipulated data array to the 'after' div.
+
